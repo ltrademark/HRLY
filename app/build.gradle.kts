43df512292641f5e66294cbbfe96ba9884f0acba
Leo H. <leo@ltrdmrk.co>
2025-12-23 23:12:21 -0500

    ✨ Adding fixed Wrokflow with reworked images + featureGraphic
    testing for F-droid build after this

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 4
-        versionName = "1.35"
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
