f2b0baeb5ece80c9a2cbbaa540c315e47551d42e
leo <leo@zoomph.com>
2026-07-12 02:57:23 -0400

    ⬆️ Bump version to 1.6
    - versionCode 5 -> 6
    - versionName 1.5 -> 1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.ltrademark.hourly"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.5"
+        versionCode = 6
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
